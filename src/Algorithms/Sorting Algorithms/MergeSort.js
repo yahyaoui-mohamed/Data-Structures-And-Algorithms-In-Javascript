24e14da1486639c9595f287bc200261dd356884d
yahyaoui-mohamed <hamayah4@gmail.com>
2023-03-24 02:20:06 +0100

    New Sorting Algorithms

--- src/Algorithms/Sorting Algorithms/MergeSort.js
+++ src/Algorithms/Sorting Algorithms/MergeSort.js
@@ -1,4 +1,14 @@
-function MergeSort(arr) {
+function merge(arr, low, high, mid) {
+
+}
+
+function MergeSort(arr, low, high) {
+  if (low < high) {
+    let mid = Math.floor((low + high) / 2);
+    MergeSort(arr, low, mid);
+    MergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+  }
   return arr;
 }
 
--- src/Algorithms/Sorting Algorithms/QuickSort.js
+++ src/Algorithms/Sorting Algorithms/QuickSort.js
@@ -1,4 +1,27 @@
-function QuickSort(arr) {
+function partition(arr, start, end) {
+  let pivot = arr[end];
+  let index = start;
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivot) {
+      let temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+      index++;
+    }
+  } 
+  let temp = arr[index];
+  arr[index] = arr[end];
+  arr[end] = temp;
+  return index;
+}
+
+function QuickSort(arr, low, high) {
+  if (low < high) {
+    let pivot = partition(arr, low, high);
+    console.log(arr);
+    QuickSort(arr, low, pivot - 1);
+    QuickSort(arr, pivot + 1, high);
+  }
   return arr;
 }
 
