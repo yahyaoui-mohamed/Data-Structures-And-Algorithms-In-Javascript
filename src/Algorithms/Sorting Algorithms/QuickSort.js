08d8079a77549e88fe9b8e85d0bc4c68003d71f0
yahyaoui-mohamed <hamayah4@gmail.com>
2023-03-25 01:15:31 +0100

    Fix QuickSort

--- src/Algorithms/Sorting Algorithms/QuickSort.js
+++ src/Algorithms/Sorting Algorithms/QuickSort.js
@@ -1,26 +1,26 @@
-function partition(arr, start, end) {
-  let pivot = arr[end];
-  let index = start;
-  for (let i = start; i < end; i++) {
-    if (arr[i] < pivot) {
+function partition(arr, left, right) {
+  let pivot = arr[right];
+  let index = left;
+  for (let i = left; i < right; ++i) {
+    if (arr[i] <= pivot) {
       let temp = arr[i];
       arr[i] = arr[index];
       arr[index] = temp;
       index++;
     }
-  } 
+  }
   let temp = arr[index];
-  arr[index] = arr[end];
-  arr[end] = temp;
+  arr[index] = arr[right];
+  arr[right] = temp;
   return index;
 }
 
-function QuickSort(arr, low, high) {
-  if (low < high) {
-    let pivot = partition(arr, low, high);
+function QuickSort(arr, left, right) {
+  if (left < right) {
+    let pivot = partition(arr, left, right);
     console.log(arr);
-    QuickSort(arr, low, pivot - 1);
-    QuickSort(arr, pivot + 1, high);
+    QuickSort(arr, left, pivot - 1);
+    QuickSort(arr, pivot + 1, right);
   }
   return arr;
 }
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import QuickSort from "./Algorithms/Sorting Algorithms/QuickSort.js";
+
+let arr = [2, 5, 7, 4, 1, 6];
+
+console.log(QuickSort(arr, 0, arr.length - 1));
